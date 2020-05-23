c8d650f889652509f2d33e324e7971d07251fc8c
RexyGreen57 <65758639+RexyGreen57@users.noreply.github.com>
2020-05-23 01:46:18 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pong</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<h1>Ping Pong</h1>
+	<canvas id="canvas"></canvas>
+	<script src="main.js"></script>
+
+</body>
+</html>
